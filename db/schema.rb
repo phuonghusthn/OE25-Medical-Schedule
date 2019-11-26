697710eafc78b1b72d2b20dd06c1cb1613f6f15f
YenNguyenBKHN <yennguyen26101998@gmail.com>
2019-11-26 13:36:06 +0700

    Show doctors 2nd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_014829) do
+ActiveRecord::Schema.define(version: 2019_11_25_071514) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
