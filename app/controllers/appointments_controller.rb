ee9631f13784d1781e4392b4d780775f221a4da5
phuonghusthn <nguyenphuongnt1997@gmail.com>
2019-11-27 16:49:42 +0700

    create request appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,31 @@
 class AppointmentsController < ApplicationController
+  before_action :logged_in_user, only: %i(create)
+
   def new
     @appointment = Appointment.new
   end
 
   def edit; end
+
+  def create
+    @appointment = current_user.appointments.build appointment_params
+    @appointment.medical_record.attach params[:appointment][:medical_record]
+    created_appointment
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit Appointment::APPOINTMENT_PARAMS
+  end
+
+  def created_appointment
+    if @appointment.save
+      flash[:success] = t "appointment_created"
+      redirect_to root_url
+    else
+      flash[:danger] = t "appointment_not_created"
+      render :new
+    end
+  end
 end
